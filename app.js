9ae267d81df29717448d8966a1b27889e8a2b15c
Santi Iraola <santi@Santis-MacBook-Pro.local>
2017-12-13 11:24:20 -0800

    React Components completed

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// TODO
+var App = () => (<div><GroceryList groceryItems={['Verduras', 'Frutas']}/></div>);
+
+class GroceryList extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	consoleClick(event) { console.log(event) }
+
+	render(){ 
+		return (
+		<ul onClick={this.consoleClick}>
+			<GroceryItem item={this.props.groceryItems[0]}/>
+			<GroceryItem item={this.props.groceryItems[1]}/>
+		</ul>);
+	}
+};
+
+class GroceryItem extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			'done': false
+		};
+		this.crossItemOnHover = this.crossItemOnHover.bind(this);
+	}
+
+	crossItemOnHover(event) {
+		this.setState({'done': !this.state.done});
+	}
+
+	render() {
+		const style = {textDecoration: this.state.done ? 'line-through' : 'none'}
+		return (<li style={style} onMouseEnter={this.crossItemOnHover}>{this.props.item}</li>);
+	}
+} 
+
+
+
+
+ReactDOM.render(<App/>, document.getElementById('app'));
